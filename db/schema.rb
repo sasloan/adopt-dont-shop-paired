3215eade0f37f0df7ebac42fb6e5be320161be16
sasloan <sebastiansloan94@gmail.com>
2020-01-28 11:58:28 -0700

    Re create database

--- db/schema.rb
+++ db/schema.rb
@@ -10,50 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20191211001119) do
+ActiveRecord::Schema.define(version: 20200128185758) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "applications", force: :cascade do |t|
-    t.string "name"
-    t.string "address"
-    t.string "city"
-    t.string "state"
-    t.integer "zip"
-    t.string "phone_number"
-    t.text "why_would_you_make_a_good_pet_owner"
-  end
-
-  create_table "pet_applications", force: :cascade do |t|
-    t.bigint "pet_id"
-    t.bigint "application_id"
-    t.index ["application_id"], name: "index_pet_applications_on_application_id"
-    t.index ["pet_id"], name: "index_pet_applications_on_pet_id"
-  end
-
-  create_table "pets", force: :cascade do |t|
-    t.string "image"
-    t.string "name"
-    t.integer "approximate_age"
-    t.string "sex"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.bigint "shelter_id"
-    t.string "description"
-    t.string "adoptable", default: "yes"
-    t.string "applicants"
-    t.index ["shelter_id"], name: "index_pets_on_shelter_id"
-  end
-
-  create_table "reviews", force: :cascade do |t|
-    t.string "title"
-    t.integer "rating"
-    t.string "content"
-    t.string "optional_picture", default: "https://dachshundjournal.com/wp-content/uploads/2018/07/dachshund-1519374_1280-900x681.jpg"
-    t.bigint "shelter_id"
-    t.index ["shelter_id"], name: "index_reviews_on_shelter_id"
-  end
 
   create_table "shelters", force: :cascade do |t|
     t.string "name"
@@ -61,12 +21,6 @@ ActiveRecord::Schema.define(version: 20191211001119) do
     t.string "city"
     t.string "state"
     t.integer "zip"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key "pet_applications", "applications"
-  add_foreign_key "pet_applications", "pets"
-  add_foreign_key "pets", "shelters"
-  add_foreign_key "reviews", "shelters"
 end
