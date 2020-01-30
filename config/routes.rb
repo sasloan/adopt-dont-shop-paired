ab79aacd45d8041c1c4243b8dffac8938e2d0415
sasloan <sebastiansloan94@gmail.com>
2020-01-30 13:42:12 -0700

    Made shelters the welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+	#root
+	get '/', to: 'shelters#index'
+
 	#shelters
 	get '/shelters', to: 'shelters#index'
 	get '/shelters/new', to: 'shelters#new'
