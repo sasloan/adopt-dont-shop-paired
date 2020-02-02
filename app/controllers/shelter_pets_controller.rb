25bbd398872e16118330b2bc1c4be9ec2e52d0c3
sasloan <sebastiansloan94@gmail.com>
2020-02-02 13:53:04 -0700

    Clean up

--- app/controllers/shelter_pets_controller.rb
+++ app/controllers/shelter_pets_controller.rb
@@ -3,6 +3,5 @@ class ShelterPetsController < ApplicationController
 	def index
 		@shelter = Shelter.find(params[:shelter_id])
 		@pets = @shelter.pets.all
-		binding.pry
 	end
 end
--- spec/features/shelter_pets/index_spec.rb
+++ spec/features/shelter_pets/index_spec.rb
@@ -4,10 +4,10 @@ describe 'As a User' do
 	describe 'when I visit the shelters pets page' do
 		before :each do
 			@aps = Shelter.create!(name: "Arvada Pet Shelter", address: "9876 Lamar Blvd.", city: "Arvada", state: "Co.", zip: "80003")
+			@ozzy = @aps.pets.create!(image: "https://www.insidedogsworld.com/wp-content/uploads/2017/06/German-Shepherd-Standard-Coat-GSC-1000x575-1-1-1-1.jpg", name: "Ozzy Paws Born", description: "German Shepard", age: 4, sex: "Male", adoptable: false)
 			@jona = @aps.pets.create!(image: "https://www.allthingsdogs.com/wp-content/uploads/2018/08/How-to-Care-for-a-Black-German-Shepherd.jpg", name: "Jona Bark", description: "Black Shepard", age: 6, sex: "Female", adoptable: true)
 			@cricket = @aps.pets.create!(image: "https://www.allthingsdogs.com/wp-content/uploads/2018/08/Breed-Standard-for-a-Black-GSD.jpg", name: "Cricket", description: "Best girl", age: 20, sex: "Female", adoptable: false)
 			@athena = @aps.pets.create!(name: "Athena", description: "Butthead", age: 1, sex: "female", adoptable: false)
-			@ozzy = @aps.pets.create!(image: "https://www.insidedogsworld.com/wp-content/uploads/2017/06/German-Shepherd-Standard-Coat-GSC-1000x575-1-1-1-1.jpg", name: "Ozzy Paws Born", description: "German Shepard", age: 4, sex: "Male", adoptable: true)
 			@acph = Shelter.create!(name: "Adams County Pet Hospital", address: "7834 Pecos St.", city: "Thornton", state: "Co.", zip: "80221")
 			@twitch = @acph.pets.create!(image: "https://i.pinimg.com/originals/6e/3c/c1/6e3cc15c678002f4ece659442ae9aefd.jpg", name: "Twitch", description: "Doxine Mini", age: 7, sex: "Male")
 		end
@@ -136,7 +136,7 @@ describe 'As a User' do
 			expect(current_path).to eq("/shelters/#{@aps.id}/pets")
 
 			expect(@aps.pets.first).to eq(@jona)
-			expect(@aps.pets.last).to eq(@cricket)
+			expect(@aps.pets.last).to eq(@ozzy)
 		end
 	end
 end
