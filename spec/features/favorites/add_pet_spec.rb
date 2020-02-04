5eef8b6399726e4f24b97b7a4d87a9f93dbf01d1
sasloan <sebastiansloan94@gmail.com>
2020-02-03 17:42:07 -0700

    Create spec to test that more than one can be added to the cart

--- spec/features/favorites/add_pet_spec.rb
+++ spec/features/favorites/add_pet_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 describe 'As a Visitor' do
-	describe "When a user adds pets to their favorites page" do
+	describe "When a visitor adds pets to their favorites page" do
 		before :each do
 			@aps = Shelter.create!(name: "Arvada Pet Shelter", address: "9876 Lamar Blvd.", city: "Arvada", state: "Co.", zip: "80003")
 			@jona = @aps.pets.create!(image: "https://www.allthingsdogs.com/wp-content/uploads/2018/08/How-to-Care-for-a-Black-German-Shepherd.jpg", name: "Jona Bark", description: "Black Shepard", age: 6, sex: "Female")
@@ -17,5 +17,25 @@ describe 'As a Visitor' do
 			expect(current_path).to eq("/pets/#{@jona.id}")
 	    expect(page).to have_content("#{@jona.name} has been added to your favorites")
 	  end
+
+		describe 'I can add multiple Pets to my favorites page' do
+
+			it 'I can see all the pets in my favorites page' do
+
+				visit "/pets/#{@jona.id}"
+
+				click_button "Add Pet To Favorites"
+
+				expect(current_path).to eq("/pets/#{@jona.id}")
+				expect(page).to have_content("#{@jona.name} has been added to your favorites")
+
+				visit "/pets/#{@ozzy.id}"
+
+				click_button "Add Pet To Favorites"
+
+				expect(current_path).to eq("/pets/#{@ozzy.id}")
+				expect(page).to have_content("#{@ozzy.name} has been added to your favorites")
+			end
+		end
 	end
 end
