e056ddb2b0ab62824171213298691e80471aa27c
sasloan <sebastiansloan94@gmail.com>
2020-02-03 17:46:18 -0700

    Complete functionality to add more than one pet to favorites

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,8 +1,13 @@
 class FavoritesController < ApplicationController
 
 	def update
-		@pet = Pet.find(params[:pet_id])
-		flash[:notice] = "#{@pet.name} has been added to your favorites"
-		redirect_to "/pets/#{@pet.id}"
+		pet = Pet.find(params[:pet_id])
+		pet_id_str = pet.id.to_s
+		session[:favorites] ||= Hash.new(0)
+		session[:favorites][pet_id_str] ||= 0
+		session[:favorites][pet_id_str] = session[:favorites][pet_id_str] + 1
+		quantity = session[:favorites][pet_id_str]
+		flash[:notice] = "#{pet.name} has been added to your favorites"
+		redirect_to "/pets/#{pet.id}"
 	end
 end
