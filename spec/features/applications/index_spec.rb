dcc81ab8cad7c61b2b9a538aaedcd9c9ccf3f805
Ben Fox <benfox1216@gmail.com>
2020-02-08 23:39:35 -0700

    Add passing test/code for making sure a message confirms if there are no applications for that pet

--- spec/features/applications/index_spec.rb
+++ spec/features/applications/index_spec.rb
@@ -5,6 +5,7 @@ describe "As a visitor" do
     before :each do
       @aps = Shelter.create!(name: "Arvada Pet Shelter", address: "9876 Lamar Blvd.", city: "Arvada", state: "Co.", zip: "80003")
       @jona = @aps.pets.create!(image: "https://www.allthingsdogs.com/wp-content/uploads/2018/08/How-to-Care-for-a-Black-German-Shepherd.jpg", name: "Jona Bark", description: "Black Shepard", age: 6, sex: "Female")
+      @athena = @aps.pets.create!(name: "Athena", description: "Butthead", age: 1, sex: "female", adoptable: true)
       @application_1 = @jona.applications.create!(name: "John Doe", address: "123 Arf St", city: "New York", state: "NY", zip: "38567", phone_number: "374-747-5842", description: "Good home")
       @application_2 = @jona.applications.create!(name: "Jane Doe", address: "456 Meow St", city: "Detroit", state: "MI", zip: "33355", phone_number: "374-747-3432", description: "Great home")
       
@@ -20,5 +21,12 @@ describe "As a visitor" do
       
       click_link "#{@application_1.name}"
       expect(current_path).to eq("/applications/#{@application_1.id}")
+    end
+    
+    it "If there are no applications I see a message confirming that" do
+      visit "/pets/#{@athena.id}"
+      click_link "Applications"
+      
+      expect(page).to have_content("There are no applications for this pet yet.")
     end
 end
